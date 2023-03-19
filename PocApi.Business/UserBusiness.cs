81c19d00f81d982e48a6ce0a731bb06aac9b3b8f
Rodrigo Quaresma <rodrigo.quaresma.souza@gmail.com>
2023-03-19 15:02:27 -0300

    Injetando a dependencia repository na business

--- PocApi.Business/UserBusiness.cs
+++ PocApi.Business/UserBusiness.cs
@@ -1,10 +1,16 @@
 ﻿using PocApi.Business.Interfaces;
+using PocApi.Data.Repositories;
 using PocApi.DTOs;
 
 namespace PocApi.Business
 {
     public class UserBusiness : IUserBusiness
     {
+        private readonly UserRepository _userRepository;
+        public UserBusiness(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
         public Task<bool> Delete(int id)
         {
             throw new NotImplementedException();
